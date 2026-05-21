8a608d1596ea2ae86db43137a33aed160993ebb5
jpereira-Dev <joao.pereira.de.oliveira@ccc.ufcg.edu.br>
2026-05-21 09:47:43 -0300

    novas questões

--- roteiros/roteiro4/roteiro4.sql
+++ roteiros/roteiro4/roteiro4.sql
@@ -21,3 +21,9 @@ SELECT Fname,Lname FROM employee WHERE Sex ='M';
 
 -- questão 8
 SELECT Fname FROM employee WHERE Sex = 'M' and Superssn IS NULL;
+
+-- questão 9
+SELECT E.Fname, S.Fname FROM employee AS E, employee AS S WHERE E.superssn = S.ssn;
+
+--questão 10
+SELECT E.Fname FROM employee AS E, employee AS S WHERE E.superssn = S.ssn and S.Fname = 'Franklin';
