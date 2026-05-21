10366006f5f58de2e21bd73d0a259360564398e7
jpereira-Dev <joao.pereira.de.oliveira@ccc.ufcg.edu.br>
2026-05-21 17:58:21 -0300

    mais questões feitas

--- roteiros/roteiro4/roteiro4.sql
+++ roteiros/roteiro4/roteiro4.sql
@@ -27,3 +27,15 @@ SELECT E.Fname, S.Fname FROM employee AS E, employee AS S WHERE E.superssn = S.s
 
 --questão 10
 SELECT E.Fname FROM employee AS E, employee AS S WHERE E.superssn = S.ssn and S.Fname = 'Franklin';
+
+--questão 11
+SELECT T.Dname, E.Dlocation FROM department AS T, dept_locations AS E;
+
+-- questão 12
+SELECT T.Dname FROM department AS T, dept_locations AS E WHERE T.Dnumber = E.Dnumber and E.Dlocation LIKE 'S%';
+
+--questão 13
+SELECT T.Fname,T.Lname,E.Dependent_name FROM employee AS T, dependent AS E WHERE E.Essn = T.ssn;
+
+--questão 14
+SELECT T.Fname ||' ' ||  T.Lname, T.salary FROM employee AS T WHERE T.salary > 50000;
