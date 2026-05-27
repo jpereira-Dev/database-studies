70fa127edca08027bdb4b17f19ec3644990f65a8
jpereira-Dev <joao.pereira.de.oliveira@ccc.ufcg.edu.br>
2026-05-27 15:55:19 -0300

    Odeio a questão 7

--- roteiros/roteiro5/roteiro5.sql
+++ roteiros/roteiro5/roteiro5.sql
@@ -25,3 +25,16 @@ ORDER BY qtd_supervisionados;
  FROM employee AS sup
  RIGHT JOIN employee AS sub ON sup.ssn = sub.superssn
  GROUP BY sup.ssn;
+
+--questão 6
+SELECT MIN(qtd) FROM (SELECT COUNT(*) AS qtd
+FROM project AS proj, works_on AS trab, employee AS func 
+WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn 
+GROUP BY Pnumber) AS qtd_funcionarios; 
+
+
+--questão 7
+SELECT proj.Pnumber AS number_project, COUNT(*) AS qtd
+FROM project AS proj, works_on AS trab, employee AS func 
+WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn 
+GROUP BY Pnumber;
