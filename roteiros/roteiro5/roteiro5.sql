93759c5af4b3e205dad459f57a89bd2d9ac41518
João Pereira <joao.pereira.de.oliveira@ccc.ufcg.edu.br>
2026-05-27 21:57:06 -0300

    roteiro 5 em andamento

--- roteiros/roteiro5/roteiro5.sql
+++ roteiros/roteiro5/roteiro5.sql
@@ -1,16 +1,17 @@
+--questão 1
+SELECT COUNT(*)
+FROM employee
+WHERE sex = 'F';
 
-
-
-
-
-
-
-
+--questão2
+SELECT AVG(salary)
+FROM employee
+WHERE address LIKE '%, TX' and sex = 'M';
 
 --questão 3
-SELECT E.superssn AS ssn_supervisor, COUNT(*) AS qtd_supervisionados 
-FROM employee AS E 
-GROUP BY E.superssn 
+SELECT E.superssn AS ssn_supervisor, COUNT(*) AS qtd_supervisionados
+FROM employee AS E
+GROUP BY E.superssn
 ORDER BY qtd_supervisionados;
 
 --questão 4
@@ -28,17 +29,17 @@ ORDER BY qtd_supervisionados;
 
 --questão 6
 SELECT MIN(qtd) FROM (SELECT COUNT(*) AS qtd
-FROM project AS proj, works_on AS trab, employee AS func 
-WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn 
-GROUP BY Pnumber) AS qtd_funcionarios; 
+FROM project AS proj, works_on AS trab, employee AS func
+WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn
+GROUP BY Pnumber) AS qtd_funcionarios;
 
 
 --questão 7
 SELECT proj.Pnumber AS number_project, COUNT(*) AS qtd
-FROM project AS proj, works_on AS trab, employee AS func 
-WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn 
+FROM project AS proj, works_on AS trab, employee AS func
+WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn
 GROUP BY Pnumber
 HAVING COUNT(*) = (SELECT MIN(qtd) FROM (SELECT COUNT(*) AS qtd
-FROM project AS proj, works_on AS trab, employee AS func 
-WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn 
+FROM project AS proj, works_on AS trab, employee AS func
+WHERE proj.Pnumber = trab.pno AND trab.Essn = func.ssn
 GROUP BY Pnumber) AS qtd_funcionarios); -- DEPOIS DESSA QUESTÃO, PENSO QUE ERA MELHOR TER FEITO MÚSICA
